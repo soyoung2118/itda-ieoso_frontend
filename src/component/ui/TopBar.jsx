499a8f8ddf8f34cdfe9e398d40093d4039f70be3
김세희 <lolry0207@sookmyung.ac.kr>
2025-01-15 00:25:56 +0900

    new branch

--- src/component/ui/TopBar.jsx
+++ src/component/ui/TopBar.jsx
@@ -6,7 +6,6 @@ export default function TopBar(){
         <Wrapper>
             <img src={Logo} style={{width: "126px", height: "33px"}}/>
             <UserContainer>
-              ㅇ
             </UserContainer>
         </Wrapper>
     )
