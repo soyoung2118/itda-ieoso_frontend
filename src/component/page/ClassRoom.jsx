e8784bc96cfe058901fc58f57ac2135cbc19be2e
sehee0207 <lolry0207@gmail.com>
2025-02-11 14:43:29 +0900

    mod: 강의실 수강 화면 타임라인 삭제

--- src/component/page/ClassRoom.jsx
+++ src/component/page/ClassRoom.jsx
@@ -22,18 +22,6 @@ export default function ClassRoom() {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     };
 
-    const TimelineItems = [
-        { time: '00:00', title: '오늘의 학습목표' },
-        { time: '03:21', title: '게임적 사회생활이란?' },
-        { time: '08:33', title: '사회화 기관의 의미와 종류' },
-        { time: '11:13', title: '사회화 기관 사례 포인트' },
-        { time: '23:31', title: '현대 사회에서의 사회생활' },
-        { time: '30:51', title: '사회현상 역사' },
-        { time: '47:38', title: '사회생활의 특징2' },
-        { time: '51:25', title: '오늘 학습내용 정리' },
-        { time: '01:00:25', title: '오늘 과제 공지' }
-    ];
-
     const curriculumItems = [
         { 
             id: 1, 
@@ -48,7 +36,9 @@ export default function ClassRoom() {
             id: 2, 
             title: '2. 한 달을 기록해요', 
             subItems: [
-                { title: '1. 효과적인 기록 법', time: '21:30', status: 'pending' }
+                { title: '1. 효과적인 기록 법', time: '21:30', status: 'pending' },
+                { title: '1. 꾸미고 싶은 타입을 정해서 다이어리를 꾸며봅시다', time: '21:30', status: 'completed' },
+                { title: '1. 꾸미고 싶은 타입을 정해서 다이어리를 꾸며봅시다', time: '21:30', status: 'completed' },
             ]
         },
         { 
@@ -86,6 +76,10 @@ export default function ClassRoom() {
             <TopBar />
             <Container>
                 <LeftSide>
+                    <TitleContainer>
+                        <MainTitle>고등학교 입학 전 마스터하는 통합사회1</MainTitle>
+                        <SubTitle>사회화 기관의 의미와 종류</SubTitle>
+                    </TitleContainer>
                     <ThumbnailContainer>
                         <img 
                             src="../../component/img/classroom/thumbnail.png"
@@ -93,10 +87,6 @@ export default function ClassRoom() {
                             className="w-full h-full object-cover"
                         />
                     </ThumbnailContainer>
-                    <TitleContainer>
-                        <MainTitle>고등학교 입학 전 마스터하는 통합사회1</MainTitle>
-                        <SubTitle>사회화 기관의 의미와 종류</SubTitle>
-                    </TitleContainer>
                 </LeftSide>
 
                 <RightSide>
@@ -106,12 +96,6 @@ export default function ClassRoom() {
                             onClick={() => setSelectedMenu('curriculum')}
                         >
                             커리큘럼
-                        </MenuButton>
-                        <MenuButton 
-                            isSelected={selectedMenu === 'timeline'}
-                            onClick={() => setSelectedMenu('timeline')}
-                        >
-                            타임라인
                         </MenuButton>
                     </MenuSelect>
                     
@@ -133,7 +117,7 @@ export default function ClassRoom() {
                                         {expandedItems.has(item.id) && item.subItems.map((subItem, subIndex) => (
                                             <SubItem key={subIndex} status={subItem.status}>
                                                 <SubItemLeft>
-                                                    <SubItemTitle><span>{truncate(subItem.title, 25)}</span></SubItemTitle>
+                                                    <SubItemTitle status={subItem.status}><span>{truncate(subItem.title, 25)}</span></SubItemTitle>
                                                     <SubItemTime>
                                                         <span className="material-icons" style={{ color: '#909090', fontSize: '13.33px',  marginRight: '3px' }}>play_circle_outline</span>
                                                         {subItem.time}
@@ -146,24 +130,6 @@ export default function ClassRoom() {
                                 ))}
                             </CurriculumList>
                         )}
-
-                        {selectedMenu === 'timeline' && (
-                            <TimelineList>
-                                {TimelineItems.map((item, index) => (
-                                    <TimelineItem key={index} isFirst={index === 0}>
-                                        <TimeText isFirst={index === 0}>{item.time}</TimeText>
-                                        <TitleText>
-                                            {item.title}
-                                            {index === 0 && (
-                                                <span className="material-icons" style={{ color: '#474747', marginLeft: "5px", fontSize: '20px' }}>
-                                                    play_circle
-                                                </span>
-                                            )}
-                                        </TitleText>
-                                    </TimelineItem>
-                                ))}
-                            </TimelineList>
-                        )}
                     </RightContainer>
                 </RightSide>
             </Container>
@@ -174,23 +140,24 @@ export default function ClassRoom() {
 const Container = styled.div`
     height: 92vh;
     display: flex;
-    background-color: #FFFFFF;
+    background-color: #F6F7F9;
 `;
 
 const LeftSide = styled.div`
-    width: 80%;
-    padding-left: 57px;
-    padding-top: 36px;
+    width: 70vw;
+    height: 92vh;
+    padding: 0px 37px;
 `;
 
 const ThumbnailContainer = styled.div`
-    width: 85%;
-    aspect-ratio: 16 / 9;
+    width: 100%;
+    height: 65vh;
     background-color: black;
 `;
 
 const TitleContainer = styled.div`
-    padding-top: 20px;
+    margin-top: 36px;
+    margin-bottom: 26px;
 `;
 
 const MainTitle = styled.div`
@@ -205,9 +172,10 @@ const SubTitle = styled.div`
 `;
 
 const RightSide = styled.div`
-    width: 35vw;
+    width: 30vw;
+    padding: 0px 15px;
     padding-top: 36px;
-    padding-right: 57px;
+    background-color: #FFFFFF;
 `;
 
 const MenuSelect = styled.div`
@@ -217,38 +185,25 @@ const MenuSelect = styled.div`
     margin-bottom: 20px;
 `;
 
-const MenuButton = styled.button`
+const MenuButton = styled.div`
     flex: 1;
-    padding: 0px 16px 16px 16px;
-    font-size: 16px;
-    font-weight: ${props => props.isSelected ? '600' : '400'};
+    padding: 13px 0px;
+    font-size: 20px;
+    font-weight: 600;
     background: none;
     border: none;
-    border-bottom: 2px solid ${props => props.isSelected ? '#000' : 'transparent'};
-    color: ${props => props.isSelected ? '#000' : '#666'};
-    cursor: ${props => props.isSelected ? 'default' : 'pointer'};
+    border-bottom: 3px solid #000
 `;
 
 const RightContainer = styled.div`
     height: 70vh;
-    overflow-y: auto;
-    padding-right: 12px;
+    overflow-y: scroll;
     margin-right: -12px;
 
-    scrollbar-width: thin;
-
     &::-webkit-scrollbar {
-        width: 8px;
+        display: none;
     }
     
-    &::-webkit-scrollbar-track {
-        background: transparent;
-    }
-    
-    &::-webkit-scrollbar-thumb {
-        background-color: #D9D9D9;
-        border-radius: 4px;
-    }
 `;
 
 const CurriculumList = styled.div`
@@ -278,7 +233,7 @@ const IconWrapper = styled.div`
 `;
 
 const ItemTitle = styled.span`
-    font-size: 15px;
+    font-size: 17px;
     font-weight: 700;
 `;
 
@@ -297,6 +252,7 @@ const SubItemLeft = styled.div`
 const SubItemTitle = styled.div`
     font-size: 15px;
     margin-bottom: 4px;
+    font-weight: ${props => props.status === 'playing' ? 800 : 400}
 `;
 
 const SubItemTime = styled.div`
@@ -304,33 +260,4 @@ const SubItemTime = styled.div`
     color: #909090;
     display: flex;
     align-items: center;
-`;
-
-const TimelineList = styled.div`
-    padding: 0 20px;
-`;
-
-const TimelineItem = styled.div`
-    display: flex;
-    padding: 15px 14px;
-    ${props => props.isFirst && `
-        background-color: #F7F7F7;
-    `}
-`;
-
-const TimeText = styled.div`
-    width: 30%;
-    font-size: 15px;
-    font-weight: 500;
-    ${props => props.isFirst && `
-        color: var(--main-color);
-    `}
-`;
-
-const TitleText = styled.div`
-    display: flex;
-    align-items: center;
-    width: 70%;
-    font-size: 15px;
-    font-weight: 500;
 `;
