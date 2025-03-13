6e161353cf677155422d551186f2bfa228e5fb22
sehee0207 <lolry0207@gmail.com>
2025-03-13 23:08:06 +0900

    mod: 정해지지 않았어요 버튼 주석처리

--- src/component/page/class/Setting.jsx
+++ src/component/page/class/Setting.jsx
@@ -313,9 +313,9 @@ export default function Setting() {
                 />
                 </TimeGroup>
 
-                <TimeGroup>
+                {/* <TimeGroup>
                   <PendingButton active={isLecturePending}>정해지지 않았어요</PendingButton>
-                </TimeGroup>
+                </TimeGroup> */}
               </HalfGroup>
               <GreyHelpText style={{marginTop: '10px'}}>
                 {isLecturePending ? (
@@ -352,9 +352,9 @@ export default function Setting() {
                 />
                 </TimeGroup> 
 
-                <TimeGroup>
+                {/* <TimeGroup>
                   <PendingButton active={isAssignmentPending}>정해지지 않았어요</PendingButton>
-                </TimeGroup>
+                </TimeGroup> */}
               </HalfGroup>
               <GreyHelpText style={{marginTop: '10px'}}>
                 {isAssignmentPending ? (
