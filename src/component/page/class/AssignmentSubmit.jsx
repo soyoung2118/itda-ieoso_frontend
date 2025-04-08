95bbad252731613cf9fd648f5718e984d38d6291
soyoung2118 <soyoung2118@gmail.com>
2025-04-08 21:41:32 +0900

    DESIGN: 강의 수강 화면 모바일 사이드바 디자인 수정 및 과제 제출 화면 디자인 추가 수정:

--- src/component/page/class/AssignmentSubmit.jsx
+++ src/component/page/class/AssignmentSubmit.jsx
@@ -221,16 +221,6 @@ const ClassAssignmentSubmit = () => {
               />
             </ClickContainer>
           </TitleContainer>
-
-          {isMobile && (
-            <MobileToggleButton type="button" onClick={toggleSidebar}>
-              {isVisible ? (
-                <CloseIcon style={{ fontSize: "2.8vh" }} />
-              ) : (
-                <MenuIcon style={{ fontSize: "2.8vh" }} />
-              )}
-            </MobileToggleButton>
-          )}
         </TopContainer>
 
         <WhiteBoxComponent>
@@ -265,6 +255,18 @@ const ClassAssignmentSubmit = () => {
             submissionType={submissionType}
             setIsDeleteModalOpen={setIsDeleteModalOpen}
           />
+        )}
+
+        {isMobile && (
+          <MobileToggleButtonWrapper>
+            <MobileToggleButton type="button" onClick={toggleSidebar}>
+              {isVisible ? (
+                <CloseIcon style={{ fontSize: "3.7vh" }} />
+              ) : (
+                <MenuIcon style={{ fontSize: "3.7vh" }} />
+              )}
+            </MobileToggleButton>
+          </MobileToggleButtonWrapper>
         )}
       </LeftSide>
 
@@ -343,6 +345,7 @@ const Container = styled.div`
   display: flex;
   margin-top: 30px;
   background-color: #f6f7f9;
+  position: relative;
 `;
 
 const LeftSide = styled.div`
@@ -382,31 +385,44 @@ const MenuContainer = styled.div`
   margin-top: 3px;
 `;
 
-const MobileToggleButton = styled.button`
+const MobileToggleButtonWrapper = styled.div`
   display: none;
 
   @media (max-width: 480px) {
     display: block;
     position: fixed;
-    bottom: 4.6%;
-    right: 5%;
-    z-index: 1300;
+    bottom: 20px;
+    right: 12%;
+    z-index: 1500;
+  }
+`;
+
+const MobileToggleButton = styled.button`
+  display: none;
+
+  @media (max-width: 480px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 9999;
+    margin-top: 2vh;
+    margin-left: auto;
     background: white;
     border: 1px solid #ccc;
     border-radius: 50%;
-    padding: 0.8vh;
-    font-size: 0.5vh;
     cursor: pointer;
     color: var(--main-color);
+    padding: 0.5vh;
   }
 `;
 
 const SidebarSlideWrapper = styled.div`
   @media (max-width: 480px) {
     position: fixed;
+    z-index: 999;
     top: 0;
     right: ${(props) => (props.show ? "0" : "-100%")};
-    width: 55%;
+    width: 75%;
     height: 100%;
     background-color: white;
     transition: right 0.3s ease-in-out;
